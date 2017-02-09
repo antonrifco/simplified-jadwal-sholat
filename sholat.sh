219290e6de12f82aa0d54e6ae004a49fd1bf54a1
antonrifco <anton.rifco@gmail.com>
2017-02-09 14:16:34 +0700

    benerin <!here|here>
    change @here to <!here|here>

--- sholat.sh
+++ sholat.sh
@@ -10,7 +10,7 @@ m=`echo $a | jq .items[0].maghrib| sed s/\"//g`
 now=`TZ=":Asia/Jakarta" date +"%l:%M %P"`
 
 if [ "$m" = "$now" ]; then
-    curl -d '{"text": "@here bos magrib boss"}' https://hooks.slack.com/services/<xxxx>/<xxxx>/<xxxx>
+    curl -d '{"text": "<!here|here> bos, magrib boss"}' https://hooks.slack.com/services/<xxxx>/<xxxx>/<xxxx>
     echo "run announcer for magrib"
 else
     echo "not run announcer for magrib"
@@ -19,7 +19,7 @@ fi
 i=`echo $a | jq .items[0].isha| sed s/\"//g`
 
 if [ "$i" = "$now" ]; then
-    curl -d '{"text": "@here bos isya boss"}' https://hooks.slack.com/services/<xxxx>/<xxxx>/<xxxx>
+    curl -d '{"text": "<!here|here> bos, isya boss"}' https://hooks.slack.com/services/<xxxx>/<xxxx>/<xxxx>
     echo "run announcer for isya"
 else
     echo "not run announcer for isya"
@@ -28,7 +28,7 @@ fi
 ash=`echo $a | jq .items[0].asr| sed s/\"//g`
 
 if [ "$ash" = "$now" ]; then
-    curl -d '{"text": "@here bos ashar boss"}' https://hooks.slack.com/services/<xxxx>/<xxxx>/<xxxx>
+    curl -d '{"text": "<!here|here> bos, ashar boss"}' https://hooks.slack.com/services/<xxxx>/<xxxx>/<xxxx>
     echo "run announcer for ashar"
 else
     echo "not run announcer for ashar"
@@ -37,7 +37,7 @@ fi
 dhuhr=`echo $a | jq .items[0].dhuhr| sed s/\"//g`
 
 if [ "$dhuhr" = "$now" ]; then
-    curl -d '{"text": "@here bos dhuhr boss"}' https://hooks.slack.com/services/<xxxx>/<xxxx>/<xxxx>
+    curl -d '{"text": "<!here|here> bos, dhuhr boss"}' https://hooks.slack.com/services/<xxxx>/<xxxx>/<xxxx>
     echo "run announcer for dhuhr"
 else
     echo "not run announcer for dhuhr"
